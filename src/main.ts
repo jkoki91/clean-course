cb6121334886d1412c353bc92dd846364dba615b
jkoki91 <>
2025-02-04 16:52:34 +0100

    curso completo

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 // import './clean-code/04-homework'
 // import './clean-code/05-dry'
 // import './clean-code/06-classes-a'
-import './clean-code/07-tarea'
+// import './clean-code/07-tarea'
+import './solid/03-liskov-a'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
